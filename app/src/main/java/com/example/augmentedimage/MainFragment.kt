0820df991fbcdc762a8ba85a40bd7f7a29db472e
Anjan <am7504@rit.edu>
2024-09-12 10:00:56 -0400

    troubleshooting ar

--- app/src/main/java/com/example/augmentedimage/MainFragment.kt
+++ app/src/main/java/com/example/augmentedimage/MainFragment.kt
@@ -2,6 +2,7 @@ package com.example.augmentedimage
 
 import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.media3.common.MediaItem
@@ -14,8 +15,11 @@ import io.github.sceneview.ar.node.AugmentedImageNode
 import io.github.sceneview.math.Position
 import io.github.sceneview.node.ModelNode
 import com.example.augmentedimage.video.ExoPlayerNode
+import kotlinx.coroutines.MainScope
 
 class MainFragment : Fragment(R.layout.fragment_main) {
+
+    var TAG = MainFragment::class.java.simpleName
 
     lateinit var sceneView: ARSceneView
 
@@ -28,16 +32,17 @@ class MainFragment : Fragment(R.layout.fragment_main) {
             configureSession { session, config ->
                 config.addAugmentedImage(
                     session, "rabbit",
-                    requireContext().assets.open("augmentedimages/rabbit.jpg")
+                    requireContext().assets.open("assets/augmentedimages/rabbit.jpg")
                         .use(BitmapFactory::decodeStream)
                 )
-                config.addAugmentedImage(
-                    session, "qrcode",
-                    requireContext().assets.open("augmentedimages/qrcode.png")
-                        .use(BitmapFactory::decodeStream)
-                )
+//                config.addAugmentedImage(
+//                    session, "qrcode",
+//                    requireContext().assets.open("augmentedimages/qrcode.png")
+//                        .use(BitmapFactory::decodeStream)
+//                )
             }
             onSessionUpdated = { session, frame ->
+                // don't think it's getting here
                 frame.getUpdatedAugmentedImages().forEach { augmentedImage ->
                     if (augmentedImageNodes.none { it.imageName == augmentedImage.name }) {
                         val augmentedImageNode = AugmentedImageNode(engine, augmentedImage).apply {
@@ -45,32 +50,35 @@ class MainFragment : Fragment(R.layout.fragment_main) {
                                 "rabbit" -> addChildNode(
                                     ModelNode(
                                         modelInstance = modelLoader.createModelInstance(
-                                            assetFileLocation = "models/rabbit.glb"
+                                            assetFileLocation = "assets/models/rabbit.glb"
                                         ),
                                         scaleToUnits = 0.1f,
                                         centerOrigin = Position(0.0f)
                                     )
                                 )
 
-                                "qrcode" -> {
-                                    addChildNode(
-                                        ExoPlayerNode(
-                                            engine = engine,
-                                            materialLoader = materialLoader,
-//                                            size = Size(x = augmentedImage.extentX, y = augmentedImage.extentZ), // When the width of the image is set
-                                            exoPlayer = ExoPlayer.Builder(requireContext()).build()
-                                                .apply {
-                                                    setMediaItem(MediaItem.fromUri("https://sceneview.github.io/assets/videos/ads/ar_camera_app_ad.mp4"))
-                                                    prepare()
-                                                    playWhenReady = true
-                                                    repeatMode = Player.REPEAT_MODE_ALL
-                                                },
-//                                            chromaKeyColor = if (chromaKey) 0x2fff19 else null, // 0x2fff19 is colorOf(0.1843f, 1.0f, 0.098f)
-                                        )
-                                    )
-                                }
+
+//                                "qrcode" -> {
+//                                    addChildNode(
+//                                        ExoPlayerNode(
+//                                            engine = engine,
+//                                            materialLoader = materialLoader,
+////                                            size = Size(x = augmentedImage.extentX, y = augmentedImage.extentZ), // When the width of the image is set
+//                                            exoPlayer = ExoPlayer.Builder(requireContext()).build()
+//                                                .apply {
+//                                                    setMediaItem(MediaItem.fromUri("https://sceneview.github.io/assets/videos/ads/ar_camera_app_ad.mp4"))
+//                                                    prepare()
+//                                                    playWhenReady = true
+//                                                    repeatMode = Player.REPEAT_MODE_ALL
+//                                                },
+////                                            chromaKeyColor = if (chromaKey) 0x2fff19 else null, // 0x2fff19 is colorOf(0.1843f, 1.0f, 0.098f)
+//                                        )
+//                                    )
+//                                }
                             }
+                            Log.d(TAG, "rabbit found");
                         }
+
                         addChildNode(augmentedImageNode)
                         augmentedImageNodes += augmentedImageNode
                     }
